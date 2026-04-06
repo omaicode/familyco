965c4be8a984bdb762d00d66ecc3766a2bdf6dfe
Bieber Kieu <kieutrongthien@gmail.com>
2026-04-06 12:55:05 +0700

    chore: update Prisma config and add better-sqlite3

--- packages/server/src/db/prisma-client.ts
+++ packages/server/src/db/prisma-client.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './prisma/client';
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL ?? 'file:../../prisma/dev.db',
+});
+
 export const prismaClient =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['error', 'warn']
+    adapter
   });
 
 if (process.env.NODE_ENV !== 'production') {
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-const databaseUrl = process.env.DATABASE_URL ?? 'file:./prisma/dev.db';
+const databaseUrl = process.env.DATABASE_URL || 'file:./prisma/dev.db';
 
 export default defineConfig({
     schema: "prisma/schema.prisma",
